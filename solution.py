6aa6d1d42e09b74e280571a84f7431b9908b33c5
vanorro228 <33964185+vanorro228@users.noreply.github.com>
2023-04-05 17:36:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,6 @@ import numpy as np
 chat_id = 5156861873 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    t = 48
+    a = x / t**2
+    return a.mean() #
