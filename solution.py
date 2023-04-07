f58d2108e9eb9cf6c406d07ba02c06c6923367b1
vanorro228 <33964185+vanorro228@users.noreply.github.com>
2023-04-07 20:47:23 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,5 +6,8 @@ chat_id = 5156861873 # Ваш chat ID, не меняйте название пе
 
 def solution(x: np.array) -> float:
     t = 48
+    n = len(x)
+    error_v = np.random.normal(0, 1, n) # ошибки
+    a = error_v/t 
     a = (np.mean(x)*2)/t*t
     return a #
